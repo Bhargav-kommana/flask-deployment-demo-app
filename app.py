65adf9a38b554efd527990f604c5c5cc922a54cb
Bhargav-kommana <149000262+Bhargav-kommana@users.noreply.github.com>
2026-01-02 22:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello AWS – This is Bhargav"
+    return "This is Bhargav"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
